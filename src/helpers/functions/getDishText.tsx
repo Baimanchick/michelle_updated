4da200759f48d465876f8e99124280b0fe40edf7
Baimanchick <baika132213@gmail.com>
2024-06-06 15:55:06 +0600

    added some functionality almost finish

---
+++ src/helpers/functions/getDishText.tsx
@@ -0,0 +1,20 @@
+import { DishesType } from "../interfaces/dishes.interface";
+import { getSelectedLanguage } from "./getSelectedLanguage";
+
+const getDishText = (dish: DishesType) => {
+    const selectedLanguage = getSelectedLanguage();
+    switch (selectedLanguage) {
+        case "Русский":
+            return dish.text_ru;
+        case "Кыргызча":
+            return dish.text_kg;
+        case "English":
+            return dish.text_en;
+        case "Türkçe":
+            return dish.text_tu;
+        default:
+            return dish.text_ru;
+    }
+};
+
+export default getDishText;
---
+++ src/helpers/functions/getDishTitle.ts
@@ -0,0 +1,20 @@
+import { DishesType } from "../interfaces/dishes.interface";
+import { getSelectedLanguage } from "./getSelectedLanguage";
+
+const getDishTitle = (dish: DishesType) => {
+  const selectedLanguage = getSelectedLanguage();
+  switch (selectedLanguage) {
+    case "Русский":
+      return dish.title_ru;
+    case "Кыргызча":
+      return dish.title_kg;
+    case "English":
+      return dish.title_en;
+    case "Türkçe":
+      return dish.title_tu;
+    default:
+      return dish.title_ru;
+  }
+};
+
+export default getDishTitle;
